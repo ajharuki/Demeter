2dd4d264b8300db5cc0e2112cd00b4972e4c7942
juanitosway <tpalomares18@gmail.com>
2017-03-11 10:10:55 -1000

    Confirmed working for a button

--- client/main.js
+++ client/main.js
@@ -1,6 +1,32 @@
-import angular from 'angular';
-import angularMeteor from 'angular-meteor';
+import { Template } from 'meteor/templating';
+//import '../imports/api/m2x.js';
+import { Session } from 'meteor/session';
+//import angular from 'angular';
+//import angularMeteor from 'angular-meteor';
 
-angular.module('demeter', [
-  angularMeteor
-]);
+// angular.module('demeter', [
+//   angularMeteor
+// ]);
+//
+//
+
+
+if (Meteor.isClient) {
+  Template.plswork.events({
+    'click button'(event, instance) {
+      // increment the counter when button is clicked
+      Meteor.call('please_work', function (err, result){
+        if(err)
+          console.log(err);
+        console.log(result);
+        Session.set('test', result);
+      })
+    },
+  });
+
+  Template.plswork.helpers({
+    test_pull: function() {
+      return Session.get('test');
+    }
+  });
+}
--- server/main.js
+++ server/main.js
@@ -1,6 +1,16 @@
 import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
 import '../imports/api/m2x.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  var apiUrl = "http://api-m2x.att.com/v2/devices/153fe019985aedf824085674c156a7bc";
+  var response = HTTP.get(apiUrl, {headers: {"X-M2X-KEY" : "f76e681663265e4c8de18b874f6dcc21"}}, function(err, response) {
+    console.log("DID STUFF");
+    console.log(response);
+
+  });
+  console.log("done");
 });
+
+
