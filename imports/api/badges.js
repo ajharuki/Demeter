9a00d024d6c83de41c7c82d431105b07d0f9486c
jlarobello <jlarobello@live.com>
2017-03-11 14:16:58 -1000

    add badge api

--- imports/api/badges.js
+++ imports/api/badges.js
@@ -5,8 +5,28 @@ import { check } from 'meteor/check';
 /**
  * API for badge queries.
  */
- export const Badges = new Mongo.Collection('badges');
+export const Badges = new Mongo.Collection('badges');
 
 Meteor.methods({
-  
+  'badges.insert' (name, desc, image_url) {
+    check(name, String);
+    check(desc, String);
+    check(image_url, String);
+
+    if (!Badges.findOne({name})) {
+      Badges.insert({
+        name,
+        desc,
+        image_url
+      })
+    }
+  },
+  'badges.getBadge' (name) {
+    check(name, String);
+
+    if (Badges.findOne({name})) {
+      return Badges.findOne({name});
+    }
+    return null;
+  }
 });
